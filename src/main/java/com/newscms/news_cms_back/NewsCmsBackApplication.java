c35800622dcbe18b358c1de8cc25a938db73d975
dongyoon7212 <dongyoon7212@naver.com>
2025-02-18 12:42:02 +0900

    init: 백엔드 초기 셋팅

---
+++ src/main/java/com/newscms/news_cms_back/NewsCmsBackApplication.java
@@ -0,0 +1,13 @@
+package com.newscms.news_cms_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsCmsBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsCmsBackApplication.class, args);
+	}
+
+}
